36304b77fe209c4a8fa4a1bf13a612d8ee89b983
Frankyston Lins <frankyston@gmail.com>
2021-11-18 20:43:02 -0300

    Added access authenticator logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def log_in(user)
+    session[:token] = user.token
+  end
+
+  def authenticate_user
+    flash[:notice] = 'Acesso não autorizado.'
+    redirect_to root_path unless current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(token: session[:token])
+  end
 end
