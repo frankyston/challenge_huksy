41150c56c1e10fe0debe3269f37747c585c308d4
Frankyston Lins <frankyston@gmail.com>
2021-12-01 11:29:44 -0300

    Missing to put the route of the previous commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/registration/new', to: 'registrations#new', as: 'new_registration'
   post '/registration', to: 'registrations#create', as: 'registration'
   get '/session/new', to: 'sessions#new', as: 'new_session'
+  delete '/session/destroy', to: 'sessions#destroy', as: 'destroy_session'
   post '/session', to: 'sessions#create', as: 'session'
   resources :invoices
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
