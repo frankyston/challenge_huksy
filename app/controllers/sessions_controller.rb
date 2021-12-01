f8b858d2f5546b474208d3295a9de546263e67a0
Frankyston Lins <frankyston@gmail.com>
2021-12-01 11:29:11 -0300

    Added session destroy; Refactory link navigations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,11 @@ class SessionsController < ApplicationController
       end
   end
 
+  def destroy
+    session.delete(:token)
+    redirect_to root_path
+  end
+
   private
 
   def error_session(error)
