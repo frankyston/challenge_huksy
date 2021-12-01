f25d54921328260bb9801fae57428de0fab831c9
Frankyston Lins <frankyston@gmail.com>
2021-12-01 16:54:47 -0300

    Added route send invoice

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get '/session/new', to: 'sessions#new', as: 'new_session'
   delete '/session/destroy', to: 'sessions#destroy', as: 'destroy_session'
   post '/session', to: 'sessions#create', as: 'session'
-  resources :invoices
+  resources :invoices do
+    member do
+      get 'send_invoice'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
