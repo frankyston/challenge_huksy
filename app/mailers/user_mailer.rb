1cdbda3a053ba8aca4173f64a65edcde0ae29d0e
Frankyston Lins <frankyston@gmail.com>
2021-12-01 16:57:38 -0300

    Create mailer send invoice

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ApplicationMailer
   def send_token(user)
     @user = user
-    mail to: user.email, subject: 'Send token for system access'
+    mail to: user.email, subject: 'Seu token de acesso ao Challenge Husky'
+  end
+
+  def send_invoice(invoice_id)
+    @invoice = Invoice.find(invoice_id)
+    mail to: @invoice.invoice_to_email, subject: "Você acabou de receber uma invoice de #{@invoice.invoice_from}"
   end
 end
